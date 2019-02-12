6a47464bb497b637f24f4a3d9ce49b38e6c781a8
Balder Van Camp <balder.vancamp@realdolmen.com>
2019-02-12 09:23:24 +0100

    initial import

---
+++ src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/WasdappCoreApplication.java
@@ -0,0 +1,14 @@
+package com.realdolmen.hbo5.wasdapp.wasdappcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasdappCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasdappCoreApplication.class, args);
+	}
+
+}
+
