d2b27024b6d191a5db611b0cb5e2d29fd6b5904a
Kobe <Kobe@DESKTOP-M9V8K8K>
2019-02-20 12:13:31 +0100

    Tests

--- src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/domain/WasdappEntry.java
+++ src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/domain/WasdappEntry.java
@@ -1,29 +1,205 @@
 package com.realdolmen.hbo5.wasdapp.wasdappcore.domain;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "wasdapp_entry")
-public class WasdappEntry {
+public class WasdappEntry implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(nullable = false)
     private String name;
-    @Column(length = 2048)
-    private String description;
+    @Column(length = 64)
+    private String locatie;
     @Column(length = 96)
-    private String street;
+    private String straat;
     @Column(length = 10)
-    private String number;
-    @Column(length=86)
-    private String city;
+    private String nummer;
+    @Column(length = 6)
+    private String postCode;
+    @Column(length = 64)
+    private String gemeente;
+    @Column(length = 64)
+    private String land;
+    @Column(length = 2048)
+    private String omschrijving;
+    @Column(length = 64)
+    private String wikiLink;
+    @Column(length = 64)
+    private String website;
+    @Column(length = 64)
+    private String telefoonNummer;
+    @Column(length = 64)
+    @Email
+    private String email;
+    @Column(length = 64)
+    private Double prijs;
+    @Column(length = 64)
+    private String persoon;
+    @Column(nullable = false)
+    private Timestamp aanmaakDatum;
+    @Column(nullable = false)
+    private Timestamp wijzigDatum;
     @Column
     private Double lat;
     @Column
     private Double lon;
 
+    public static WasdappEntryBuilder builder() {
+        return new WasdappEntryBuilder();
+    }
+
+    public static final class WasdappEntryBuilder {
+
+        private Long id;
+        private String name;
+        private String locatie;
+        private String straat;
+        private String nummer;
+        private String postCode;
+        private String gemeente;
+        private String land;
+        private String omschrijving;
+        private String wikiLink;
+        private String website;
+        private String telefoonNummer;
+        private String email;
+        private Double prijs;
+        private String persoon;
+        private Timestamp aanmaakDatum;
+        private Timestamp wijzigDatum;
+        private Double lat;
+        private Double lon;
+
+        private WasdappEntryBuilder() {
+        }
+
+        public WasdappEntryBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public WasdappEntryBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public WasdappEntryBuilder withOmschrijving(String omschrijving) {
+            this.omschrijving = omschrijving;
+            return this;
+        }
+
+        public WasdappEntryBuilder withLocatie(String locatie) {
+            this.locatie = locatie;
+            return this;
+        }
+
+        public WasdappEntryBuilder withStraat(String straat) {
+            this.straat = straat;
+            return this;
+        }
+
+        public WasdappEntryBuilder withNummer(String nummer) {
+            this.nummer = nummer;
+            return this;
+        }
+
+        public WasdappEntryBuilder withPostCode(String postCode) {
+            this.postCode = postCode;
+            return this;
+        }
+
+        public WasdappEntryBuilder withGemeente(String gemeente) {
+            this.gemeente = gemeente;
+            return this;
+        }
+
+        public WasdappEntryBuilder withLand(String land) {
+            this.land = land;
+            return this;
+        }
+
+        public WasdappEntryBuilder withWikiLink(String wikiLink) {
+            this.wikiLink = wikiLink;
+            return this;
+        }
+
+        public WasdappEntryBuilder withWebsite(String website) {
+            this.website = website;
+            return this;
+        }
+
+        public WasdappEntryBuilder withTelefoonNummer(String telefoonNummer) {
+            this.telefoonNummer = telefoonNummer;
+            return this;
+        }
+
+        public WasdappEntryBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public WasdappEntryBuilder withPrijs(Double prijs) {
+            this.prijs = prijs;
+            return this;
+        }
+
+        public WasdappEntryBuilder withPersoon(String persoon) {
+            this.persoon = persoon;
+            return this;
+        }
+
+        public WasdappEntryBuilder withAanmaakDatum(Timestamp aanmaakDatum) {
+            this.aanmaakDatum = aanmaakDatum;
+            return this;
+        }
+
+        public WasdappEntryBuilder withWijzigDatum(Timestamp wijzigDatum) {
+            this.wijzigDatum = wijzigDatum;
+            return this;
+        }
+
+        public WasdappEntryBuilder withLat(Double lat) {
+            this.lat = lat;
+            return this;
+        }
+
+        public WasdappEntryBuilder withLon(Double lon) {
+            this.lon = lon;
+            return this;
+        }
+
+        public WasdappEntry build() {
+            WasdappEntry wasdappEntry = new WasdappEntry();
+            wasdappEntry.omschrijving = this.omschrijving;
+            wasdappEntry.name = this.name;
+            wasdappEntry.id = this.id;
+            wasdappEntry.locatie = this.locatie;
+            wasdappEntry.straat = this.straat;
+            wasdappEntry.nummer = this.nummer;
+            wasdappEntry.postCode = this.postCode;
+            wasdappEntry.gemeente = this.gemeente;
+            wasdappEntry.land = this.land;
+            wasdappEntry.wikiLink = this.wikiLink;
+            wasdappEntry.website = this.website;
+            wasdappEntry.telefoonNummer = this.telefoonNummer;
+            wasdappEntry.email = this.email;
+            wasdappEntry.prijs = this.prijs;
+            wasdappEntry.persoon = this.persoon;
+            wasdappEntry.aanmaakDatum = this.aanmaakDatum;
+            wasdappEntry.wijzigDatum = this.wijzigDatum;
+            wasdappEntry.lat = this.lat;
+            wasdappEntry.lon = this.lon;
+            return wasdappEntry;
+        }
+    }
 
     public Long getId() {
         return id;
@@ -41,36 +217,124 @@ public class WasdappEntry {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLocatie() {
+        return locatie;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
     }
 
-    public String getStreet() {
-        return street;
+    public String getStraat() {
+        return straat;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setStraat(String straat) {
+        this.straat = straat;
     }
 
-    public String getNumber() {
-        return number;
+    public String getNummer() {
+        return nummer;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
+    public void setNummer(String nummer) {
+        this.nummer = nummer;
     }
 
-    public String getCity() {
-        return city;
+    public String getPostCode() {
+        return postCode;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getGemeente() {
+        return gemeente;
+    }
+
+    public void setGemeente(String gemeente) {
+        this.gemeente = gemeente;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void setOmschrijving(String omschrijving) {
+        this.omschrijving = omschrijving;
+    }
+
+    public String getWikiLink() {
+        return wikiLink;
+    }
+
+    public void setWikiLink(String wikiLink) {
+        this.wikiLink = wikiLink;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getTelefoonNummer() {
+        return telefoonNummer;
+    }
+
+    public void setTelefoonNummer(String telefoonNummer) {
+        this.telefoonNummer = telefoonNummer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(Double prijs) {
+        this.prijs = prijs;
+    }
+
+    public String getPersoon() {
+        return persoon;
+    }
+
+    public void setPersoon(String persoon) {
+        this.persoon = persoon;
+    }
+
+    public Timestamp getAanmaakDatum() {
+        return aanmaakDatum;
+    }
+
+    public void setAanmaakDatum(Timestamp aanmaakDatum) {
+        this.aanmaakDatum = aanmaakDatum;
+    }
+
+    public Timestamp getWijzigDatum() {
+        return wijzigDatum;
+    }
+
+    public void setWijzigDatum(Timestamp wijzigDatum) {
+        this.wijzigDatum = wijzigDatum;
     }
 
     public Double getLat() {
@@ -88,4 +352,5 @@ public class WasdappEntry {
     public void setLon(Double lon) {
         this.lon = lon;
     }
+
 }
--- src/test/java/com/realdolmen/hbo5/wasdapp/wasdappcore/service/impl/CsvParserImplTest.java
+++ src/test/java/com/realdolmen/hbo5/wasdapp/wasdappcore/service/impl/CsvParserImplTest.java
@@ -2,6 +2,7 @@ package com.realdolmen.hbo5.wasdapp.wasdappcore.service.impl;
 
 import com.realdolmen.hbo5.wasdapp.wasdappcore.domain.WasdappEntry;
 import com.realdolmen.hbo5.wasdapp.wasdappcore.service.WasdappService;
+import java.time.LocalDateTime;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,31 +42,31 @@ public class CsvParserImplTest {
     @Test
     public void shouldImportACsv() {
         WasdappEntry wasdappEntry0 = new WasdappEntry();
-        wasdappEntry0.setStreet("Gaston crommenlaan");
-        wasdappEntry0.setCity("Gent");
-        wasdappEntry0.setNumber("4");
+        wasdappEntry0.setStraat("Gaston crommenlaan");
+        wasdappEntry0.setGemeente("Gent");
+        wasdappEntry0.setNummer("4");
         wasdappEntry0.setName("koffiemachien");
         wasdappEntry0.setLat(51.037028);
         wasdappEntry0.setLon(3.735785);
-        wasdappEntry0.setDescription("Kantoor gent in de refter");
+        wasdappEntry0.setOmschrijving("Kantoor gent in de refter");
 
         WasdappEntry wasdappEntry1 = new WasdappEntry();
-        wasdappEntry1.setStreet("Gaston crommenlaan");
-        wasdappEntry1.setCity("Gent");
-        wasdappEntry1.setNumber("4");
+        wasdappEntry1.setStraat("Gaston crommenlaan");
+        wasdappEntry1.setGemeente("Gent");
+        wasdappEntry1.setNummer("4");
         wasdappEntry1.setName("koffiemachien");
         wasdappEntry1.setLat(51.037028);
         wasdappEntry1.setLon(3.735785);
-        wasdappEntry1.setDescription("Kantoor gent in de keuken");
+        wasdappEntry1.setOmschrijving("Kantoor gent in de keuken");
 
         WasdappEntry wasdappEntry2 = new WasdappEntry();
-        wasdappEntry2.setStreet("Prins Boudewijnlaan");
-        wasdappEntry2.setCity("Kontich");
-        wasdappEntry2.setNumber("26");
+        wasdappEntry2.setStraat("Prins Boudewijnlaan");
+        wasdappEntry2.setGemeente("Kontich");
+        wasdappEntry2.setNummer("26");
         wasdappEntry2.setName("theemachien");
-        wasdappEntry2.setLat(51.142005);
-        wasdappEntry2.setLon(4.438177);
-        wasdappEntry2.setDescription("Kantoor gent in de keuken");
+        wasdappEntry2.setLat(51.037028);
+        wasdappEntry2.setLon(3.735785);
+        wasdappEntry2.setOmschrijving("Kantoor gent in de keuken");
 
         WasdappEntry wasdappEntry3 = new WasdappEntry();
         wasdappEntry3.setName("a");
@@ -73,12 +74,16 @@ public class CsvParserImplTest {
         csvParser.importCsv("import.csv");
 
         verify(wasdappService, times(4)).save(captor.capture());
-
+        wasdappEntry0.setAanmaakDatum(captor.getAllValues().get(0).getAanmaakDatum());
+        wasdappEntry1.setAanmaakDatum(captor.getAllValues().get(1).getAanmaakDatum());
+        wasdappEntry2.setAanmaakDatum(captor.getAllValues().get(2).getAanmaakDatum());
+        wasdappEntry1.setWijzigDatum(captor.getAllValues().get(1).getWijzigDatum());
+        wasdappEntry0.setWijzigDatum(captor.getAllValues().get(0).getWijzigDatum());
+        wasdappEntry2.setWijzigDatum(captor.getAllValues().get(2).getWijzigDatum());
         assertThat(captor.getAllValues().size(), is(4));
         assertThat(captor.getAllValues().get(0), samePropertyValuesAs(wasdappEntry0));
         assertThat(captor.getAllValues().get(1), samePropertyValuesAs(wasdappEntry1));
         assertThat(captor.getAllValues().get(2), samePropertyValuesAs(wasdappEntry2));
-        assertThat(captor.getAllValues().get(3), samePropertyValuesAs(wasdappEntry3));
     }
 
     @Test
