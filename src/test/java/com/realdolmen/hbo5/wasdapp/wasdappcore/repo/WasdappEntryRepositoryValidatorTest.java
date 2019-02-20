1ee53784fb1d77b7cc5d7a5858b4c7cdd9889f89
Kobe <Kobe@DESKTOP-M9V8K8K>
2019-02-20 14:40:37 +0100

    Tests

---
+++ src/test/java/com/realdolmen/hbo5/wasdapp/wasdappcore/repo/WasdappEntryRepositoryValidatorTest.java
@@ -0,0 +1,46 @@
+package com.realdolmen.hbo5.wasdapp.wasdappcore.repo;
+
+import com.realdolmen.hbo5.wasdapp.wasdappcore.domain.WasdappEntry;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WasdappEntryRepositoryValidatorTest {
+
+    private Validator validator;
+            
+    @Before
+    public void init() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void saveEntryValidatorTest() {
+        Timestamp ts = Timestamp.valueOf(LocalDateTime.now());
+        WasdappEntry entry = new WasdappEntry();
+        entry.setId(8L);
+        entry.setName("Zetel");
+        entry.setAanmaakDatum(ts);
+        entry.setWijzigDatum(ts);
+        Set<ConstraintViolation<WasdappEntry>> validatorResponse = validator.validate(entry);
+        assertEquals(validatorResponse.size(), 0);
+    }
+
+    @Test
+    public void saveWrongEntryValidatorTest() {
+        WasdappEntry entry = new WasdappEntry();
+        Set<ConstraintViolation<WasdappEntry>> validatorResponse = validator.validate(entry);
+        assertEquals(validatorResponse.size(), 4);
+    }
+}
