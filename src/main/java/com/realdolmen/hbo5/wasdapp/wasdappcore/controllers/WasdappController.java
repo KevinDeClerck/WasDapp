bca0c7c1844717cdccdd4b90481feda0266a0379
JasperVB3 <jasper.vandenbossche@hormail.com>
2019-02-22 11:52:24 +0100

    Language

--- src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/controllers/WasdappController.java
+++ src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/controllers/WasdappController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,11 @@ public class WasdappController {
 
     @RequestMapping(value = "/wasdapp", method = RequestMethod.GET)
     public String showList(Model model) {
+        model.addAttribute("entries", wasdappService.findAll());
+        return "wasdapp.xhtml";
+    }
+    @GetMapping("wasdappinternational")
+    public String getInternationalPage(Model model) {
         model.addAttribute("entries", wasdappService.findAll());
         return "wasdapp.xhtml";
     }
