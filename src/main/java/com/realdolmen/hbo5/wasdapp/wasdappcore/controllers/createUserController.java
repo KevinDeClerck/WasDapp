5e4eb9627d3ce3a0e11db3d28edfb67ca664b57f
Kobe Van Schepdael <kobe.vanschepdael@gmail.com>
2019-05-22 07:33:52 +0000

    Update createUserController.java

--- src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/controllers/createUserController.java
+++ src/main/java/com/realdolmen/hbo5/wasdapp/wasdappcore/controllers/createUserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class createUserController {
+public class CreateUserController {
 
     @Autowired
     CurrentUser currentUser;
